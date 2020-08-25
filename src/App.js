2db69a137803cb5238eef64cfc1442752e4f0fa1
Yuliya Payevska <y.payevska@gmail.com>
2020-08-25 19:46:39 +0300

    fill actions.js and reducer.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import {createStore} from 'redux';
+import reducer from './reducer'
+// initial store
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0
+};
+
+const store = createStore(reducer, initialStore);
+
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()}/>
       <CartContainer cart={cartItems} />
     </main>
   );
