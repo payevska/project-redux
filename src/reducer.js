6589ab0d9c26e086bc6cc6569031f47b65026975
Yuliya Payevska <y.payevska@gmail.com>
2020-08-27 16:22:56 +0300

    added action for REMOVE, INCREASE and DICREASE in reducer.js and CartItem.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,51 @@
-import {DECREASE,INCREASE} from './actions'
+import {DECREASE, INCREASE, CLEAR_CART, REMOVE} from './actions'
+import cartItems from './cart-items';
 
 
-function reducer(state, action) {
-    
-    return state;
+function reducer(state, action) { 
+
+  if(action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
   }
-  
-  export default reducer;
+  if(action.type === DECREASE) {
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter(cartItem => cartItem.id !== action.payload.id);
+    }
+    else {
+      tempCart = state.cart.map(cartItem => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }  
+    return {...state, cart: tempCart};
+  }
+  if(action.type === INCREASE) {
+    let tempCart = state.cart.map(cartItem => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return {...state, cart: tempCart};
+  }
+  if(action.type === REMOVE) {
+    return {
+      ...state,
+      cart: state.cart.filter(cartItem => cartItem.id !== action.payload.id)
+    };
+  }
+  return state;
+
+}
+
+export default reducer;
+
+  /* switch(action.type) {
+    case CLEAR_CART:
+      return { ...state, cart: [] };
+    default: 
+      return state;
+  } */
